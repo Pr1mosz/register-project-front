abe60e1c5c2a0e2735fd60d83eddb4c64e6bc9d0
Pr1mosz <olechprzemyslaw@gmail.com>
2023-11-09 14:41:46 +0100

    Added SingleCompetitionView with list of all Competitors on that view.

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import './App.css';
 import {Header} from "./components/Header/Header";
 import {CompetitionView} from "./views/CompetitionView";
 import {Route, Routes} from "react-router-dom";
+import {SingleCompetitionView} from "./views/SingleCompetitionView";
 
 export const App = () => {
     return (
@@ -11,8 +12,8 @@ export const App = () => {
             <Header/>
             <Routes>
                 <Route path="/" element={<CompetitionView/>} />
+                <Route path="/:idOfCompetition" element={<SingleCompetitionView/>} />
             </Routes>
-
         </>
     );
 };
---
+++ src/components/Competitor/CompetitorTable.tsx
@@ -0,0 +1,31 @@
+import React, {useState} from 'react';
+
+import { SimpleCompetitorEntity } from "types";
+import {CompetitorTableRow} from "./CompetitorTableRow";
+import "./CompetitorTable.css"
+
+interface Props {
+    competitorsList: SimpleCompetitorEntity[];
+}
+export const CompetitorTable = (props: Props) => {
+    return <>
+        <table>
+            <thead>
+            <tr>
+                <th>Imię</th>
+                <th>Nazwisko</th>
+                <th>Płeć</th>
+                <th>Miasto</th>
+                <th>Klub</th>
+            </tr>
+            </thead>
+            <tbody>
+                {
+                    props.competitorsList.map(competitor => (
+                        <CompetitorTableRow competitor={competitor} key={competitor.id}/>
+                        ))
+                }
+            </tbody>
+        </table>
+    </>
+}
